a44ba5e7aed2857fd5c2a3462be39e426605aa4b
Duncan Beggs <Duncan.Beggs@hitachiconsulting.com>
2018-06-06 12:08:37 -0400

    added functionality to grab dest path

--- WPD/Program.cs
+++ WPD/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,11 @@ namespace WPD
     {
         static void Main(string[] args)
         {
-            var collection = new PortableDeviceCollection();
+            //get the target directory, the destination to copy the video to
+            string destPath = getFileDestinationPath();
+            Console.WriteLine(destPath);
 
+            var collection = new PortableDeviceCollection();
             collection.Refresh();
 
             //loop through all devices connected
@@ -26,7 +30,7 @@ namespace WPD
                 {
 
                     Console.WriteLine(item.Name);
-                    DisplayObject(item, device);
+                    DisplayObject(item, device, destPath);
                     if ( item is PortableDeviceFile )
                     {
                         Console.WriteLine("Copying file " + item.Name);
@@ -41,32 +45,48 @@ namespace WPD
             Console.ReadKey();
         }
 
-        public static void DisplayObject(PortableDeviceObject portableDeviceObject, PortableDevice device)
+        public static string getFileDestinationPath()
+        {
+            string line;
+            try
+            {
+                StreamReader sr = new StreamReader("destPath.txt");
+                line = sr.ReadLine();
+                return line;
+            } catch
+            {
+                Console.WriteLine("ERROR: could not find file destPath.txt to grab video destination path");
+                return null;
+            }
+            
+        }
+
+        public static void DisplayObject(PortableDeviceObject portableDeviceObject, PortableDevice device, string destPath)
         {
             Console.WriteLine(portableDeviceObject.Name);
             if (portableDeviceObject is PortableDeviceFolder)
             {
                 Console.WriteLine("Folder");
-                DisplayFolderContents((PortableDeviceFolder)portableDeviceObject, device);
+                DisplayFolderContents((PortableDeviceFolder)portableDeviceObject, device, destPath);
             }
 
         }
 
         //allows for recursion through folder structure
-        public static void DisplayFolderContents(PortableDeviceFolder folder, PortableDevice device)
+        public static void DisplayFolderContents(PortableDeviceFolder folder, PortableDevice device, string destPath)
         {
             foreach(var item in folder.Files)
             {
                 Console.WriteLine(item.Id);
                 if (item is PortableDeviceFolder)
                 {
-                    DisplayFolderContents((PortableDeviceFolder)item, device);
+                    DisplayFolderContents((PortableDeviceFolder)item, device, destPath);
                 }
 
                 if (item is PortableDeviceFile && item.Id.Contains(".mp4"))
                 {
                     Console.WriteLine("Copying video file...");
-                    device.DownloadFile((PortableDeviceFile)item, @"D:\Users\Duncan\Videos\Hitachi Videos");
+                    device.DownloadFile((PortableDeviceFile)item, destPath);
                 }
             }
         }
